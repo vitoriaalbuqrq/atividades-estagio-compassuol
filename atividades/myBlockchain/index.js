3ed462338c6c37bb6594ff3fbfbaed86333ebbf8
Vitoria Albuquerque <albuquerquevitoria14@gmail.com>
2024-11-07 22:08:36 -0300

    feat: adiciona proof of work, valida endereços e cria histórico de transações por endereço.

--- atividades/myBlockchain/index.js
+++ atividades/myBlockchain/index.js
@@ -3,21 +3,56 @@ const Transaction = require('./transaction');
 
 const myBlockchain = new Blockchain();
 
-console.log('-------------------- CRIANDO A BLOCKCHAIN --------------------');
+function main(){
+    console.log('\n--------------------------------- CRIANDO A BLOCKCHAIN ---------------------------------');
+    createTransactions();
+    displayBlockchain();
+    console.log('Blockchain é válida?', myBlockchain.isValid());
+    displayTransactionHistory('0x0012a536b1d9')
+    testBlockchainCorruption();
+}
 
-myBlockchain.addTransaction(new Transaction('0x001', '0x002', 50));
-myBlockchain.addTransaction(new Transaction('0x002', '0x003', 20));
-myBlockchain.addBlock();
+function createTransactions(){
+    try {
+        myBlockchain.addTransaction(new Transaction('0x0012a536b1d9', '0x0045d9va80v6', 50));
+        myBlockchain.addTransaction(new Transaction('0x0012a536b1d9', '0x006325dsa253', 20));
+        myBlockchain.addBlock();
+        
+        myBlockchain.addTransaction(new Transaction('0x00df5d62fd13', '0x0012a536b1d9', 10));
+        myBlockchain.addBlock();
+    } catch (error){
+        console.log("Erro ao adicionar transação:", error.message);
+    }
+}
 
-myBlockchain.addTransaction(new Transaction('0x003', '0x001', 10));
-myBlockchain.addBlock();
+function displayBlockchain(){
+    console.log(myBlockchain.blocks.map(block => block.toString())
+    .join("\n-----------------------------------------------------------------------------------------\n"));
+    console.log('-----------------------------------------------------------------------------------------');
+}
 
-console.log('Blockchain é válida?', myBlockchain.isValid());
+function displayTransactionHistory(address){
+    console.log('\n-------------------------------- HISTÓRICO DE TRANSAÇÕES --------------------------------');
+    const transactionHistory = myBlockchain.getTransactionsByAddress(address);
 
-console.log('--------------------------------------------------------------');
-console.log(myBlockchain.blocks.map(block => block.toString())
-    .join("\n--------------------------------------------------------------\n"));
-console.log('--------------------------------------------------------------');
-console.log('Teste de alteração dos dados...');
-myBlockchain.blocks[1].data[0].receiver = '0x004'; 
-console.log('Blockchain válida após alteração?', myBlockchain.isValid());
+    function displayTransactions(type, transactions){
+        if (transactions.length > 0){
+            console.log(`\n---------------------------------- Transações ${type} ----------------------------------`);
+            transactions.forEach(tx => {
+                console.log(`Sender: ${tx.sender} | Receiver: ${tx.receiver} | Amount: ${tx.amount} | Timestamp: ${tx.timestamp}`);
+            })
+        } else {
+            console.log(`Nenhuma transação ${type} com esse endereço.`);
+        }
+    }
+    displayTransactions('enviadas', transactionHistory.transactionsSent);
+    displayTransactions('recebidas', transactionHistory.transactionReceived);
+}
+
+function testBlockchainCorruption() {
+    console.log('Teste de alteração dos dados...');
+    myBlockchain.blocks[1].data[1].receiver = '0x005f682a36b7'; 
+    console.log('Blockchain válida após alteração?', myBlockchain.isValid());   
+}
+
+main();
--- atividades/myBlockchain/transaction.js
+++ atividades/myBlockchain/transaction.js
@@ -1,8 +1,17 @@
 class Transaction {
   constructor(sender, receiver, amount) {
+    if (!this.isValidAddress(sender) || !this.isValidAddress(receiver)) {
+      throw new Error("Endereço de origem ou de destino inválido!");
+    }
     this.sender = sender;
     this.receiver = receiver;
     this.amount = amount;
+    this.timestamp = Date.now();
+  }
+
+  isValidAddress(address) {
+    const adressFormat = /^0x00[a-zA-Z0-9]{10}$/;
+    return adressFormat.test(address);
   }
 }
 
